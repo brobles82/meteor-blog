d661c0f110b500629e898d1196b80f511d7cc72c
Kyle Finley <kyle.finley@gmail.com>
2012-12-04 09:30:24 -0600

    started error handling

---
+++ models/errors.js
@@ -0,0 +1,8 @@
+// Client only collection
+var Errors = new Meteor.Collection(null);
+
+var throwError = function (message, type) {
+	type = (typeof type === 'undefined') ? 'error' : type;
+	// Store errors in the 'Errors' local collection
+	Errors.insert({message: message, type: type, seen: false, show: true});
+};
